6fbaf622add2b8b8ab89b473a28632f084809005
mwoods15 <michael.stuart.woods@gmail.com>
2026-01-08 13:43:02 +0000

    Added OutputHead dense layer
    To be used to get output predictions from each hidden layer to form
    final prediction

--- models/layers.py
+++ models/layers.py
@@ -119,3 +119,13 @@ class SparseBiologicalLayer(nn.Module):
 
         return output
 
+class OutputHead(nn.Module):
+    """
+    Dense layer for predictions from any hidden layer
+    """
+    def __init__(self, input_size):
+        super(OutputHead, self).__init__()
+        self.linear = nn.Linear(input_size, 1)
+    
+    def forward(self, x):
+        return self.linear(x)
